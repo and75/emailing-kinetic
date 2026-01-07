c776515867eece644b4e942622fcbcb504e8250a
Porcella Andrea <andreaporcella@gmail.com>
2026-01-07 13:46:40 +0100

    Version A

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,49 @@
-const { src, dest, watch, series } = require('gulp');
+const { src, dest, watch, series, parallel } = require('gulp');
 const fileInclude = require('gulp-file-include');
+const htmlmin = require('gulp-htmlmin');
 
 const paths = {
-  template: 'src/template.html',
+  html: 'src/index.html',
   components: 'src/components/**/*.html',
-  output: 'src',
+  css: 'src/styles.css',
+  images: 'src/images/**/*',
+  output: 'dist',
 };
 
 function html() {
-  return src(paths.template)
+  return src(paths.html)
     .pipe(
       fileInclude({
-        basepath: 'src/components',
+        basepath: 'src',
         prefix: '@@',
         indent: true,
       })
     )
+    // .pipe(
+    //   htmlmin({
+    //     collapseWhitespace: true,
+    //     removeComments: true,
+    //     minifyCSS: true,
+    //     minifyJS: true,
+    //     ignoreCustomFragments: [/{%[\s\S]*?%}/, /{{[\s\S]*?}}/],
+    //   })
+    // )
     .pipe(dest(paths.output));
 }
 
+function images() {
+  // Copy binaries without UTF-8 re-encoding (avoid corrupting PNGs)
+  return src(paths.images, { encoding: false }).pipe(
+    dest(`${paths.output}/images`)
+  );
+}
+
 function watcher() {
-  watch([paths.template, paths.components], html);
+  watch([paths.html, paths.components, paths.css], html);
+  watch(paths.images, images);
 }
 
 exports.html = html;
-exports.watch = series(html, watcher);
-exports.default = html;
+exports.images = images;
+exports.watch = series(parallel(html, images), watcher);
+exports.default = parallel(html, images);
